bfd5944c9a3e7a2c402d81bf19858e95b617bc0e
xiaoyu <xiaoyu@guanghe.tv>
2020-11-11 14:17:53 +0800

    translate app

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,40 @@
+extern crate clap;
 use std::fs;
 use std::collections::HashMap;
+use clap::App;
 
 fn main() {
+    App::new("zhengma").version("v1.0-beta").get_matches();
+}
+
+fn init_data() {
 
     let contents = fs::read_to_string("./data/zhengma.dict.yaml")
         .expect("Something went wrong reading the file");
 
     let v: Vec<&str> = contents.split("...").collect();
     let zhengma_str = v[1];
-    let mut zhengma_dict = HashMap::new();
-    for (_, line) in zhengma_str.lines().enumerate() {
+    let mut zhengma_content: String = "".to_owned();
+    
+    for line in zhengma_str.lines() {
         let dict: Vec<&str> = line.split("\t").collect();
-        if dict.len() > 1 && !zhengma_dict.contains_key(dict[0]){
-            zhengma_dict.insert(dict[0].to_string(), dict[1].to_string());
+        if dict.len() > 1 {
+            let mut newline: String = "".to_owned();
+            newline.push_str(dict[0]);
+            newline.push(',');
+            newline.push_str(dict[1]);
+            newline.push(',');
+            newline.push_str(dict[2]);
+            newline.push('\n');
+            zhengma_content.push_str(&newline);
         }
     }
 
-    match fs::write("./data/zhengma.data", to_format_string(&zhengma_dict).as_bytes()) {
+    match fs::write("./data/zhengma.data", zhengma_content.as_bytes()) {
         Ok(()) => println!("write, success"),
         Err(e) => println!("write to file error,{}", e),
     }
 }
-
 
 fn to_format_string(dict: &HashMap<String,String>) ->  Box<String> {
     let mut contents: String = "".to_owned();
