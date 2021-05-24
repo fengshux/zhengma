515332df87c5b153c911b52963a9eeee406842e4
xiaoyu <xiaoyu@guanghe.tv>
2021-05-24 13:22:27 +0800

    trans 优化

--- src/bin/zhengma.rs
+++ src/bin/zhengma.rs
@@ -29,7 +29,7 @@ fn main() {
         .get_matches();
 
     if let Some(world) = matches.value_of("WORLD") {
-        match search_code("./data/zhengma.hash", world) {           
+        match search_code("./data/zhengma.hash", world) {
             Some(codes) =>{
                 println!("{}:{}", world, codes)
             }
@@ -40,19 +40,16 @@ fn main() {
     }
 
     
-    // let file = matches.value_of("INPUT").unwrap();
     if let Some(matches) = matches.subcommand_matches("trans") {
-        // let file = matches.value_of("INPUT").expect("expect input file");
-        // let contents = fs::read_to_string(file)
-        //     .expect("Something went wrong reading the file");
-    
-        // let dict =  get_full_code_map();
-        // let coded = to_code(dict, &contents);
-        // match matches.value_of("OUTPUT") {
-        //     Some(path) => fs::write(path, coded.as_bytes()).expect("write file error"),
-        //     None => println!("{}",coded),
-        // };
-        println!("正在维护");
+        let file = matches.value_of("INPUT").expect("expect input file");
+        let contents = fs::read_to_string(file)
+            .expect("Something went wrong reading the file");
+            
+        let coded = translate_with_cache("./data/zhengma.hash", &contents);
+        match matches.value_of("OUTPUT") {
+            Some(path) => fs::write(path, coded.as_bytes()).expect("write file error"),
+            None => println!("{}",coded),
+        };
     } 
 }
 
@@ -94,15 +91,40 @@ fn search_code(path: &str, key: &str) -> Option<String> {
     }
 }
 
-// fn to_code(dict: Box<HashMap<String,Vec<String>>>, contents: &str) -> Box<String>{
-//     let mut coded_content = Box::new("".to_owned());
-//     for v in contents.chars() {
-//         coded_content.push(v);
-//         match dict.get(&v.to_string()) {
-//             Some(code) => coded_content.push_str(&format!("({})",code.iter().max_by_key(|x| x.len()).unwrap())),
-//             None => (),
-//         };
-        
-//     }
-//     return coded_content
-// }
+fn translate_with_cache(path: &str, contents: &str) -> Box<String>{
+    let mut coded_content = Box::new("".to_owned());
+    let mut cache: HashMap<String, String> = HashMap::new();
+    
+    let hash_text = fs::read_to_string(path)
+        .expect("Something went wrong reading the file");    
+    let hash_array: Vec<&str> = hash_text.lines().collect();
+
+    for v in contents.chars() {
+        coded_content.push(v);
+
+        match cache.get(&v.to_string()) {
+            Some(code) => {
+                coded_content.push_str(&format!("({})",code))
+            }
+            None => {
+                let index = count_hash_index(&v.to_string());
+                if let Some(line) = hash_array.get(index) {
+                    if line != &"" {
+                        let cells: Vec<&str> = line.split(";").collect();
+                        for cell in cells {
+                            let code: Vec<&str> = cell.split(":").collect();
+                            if code[0] == &v.to_string() {
+                                let codes: Vec<&str> = code[1].split(",").collect();
+                                let code = codes.iter().max_by_key(|c| c.len()).unwrap();
+                                coded_content.push_str(&format!("({})",code.to_string()));
+                                cache.insert(v.to_string(), code.to_string());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }   
+   
+    return coded_content
+}
